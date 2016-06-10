43d93c2cc67f9223326fdd00de12c2b464ab5256
Sean Parsons <sean@labelinteractive.com>
2016-06-10 09:49:30 -0400

    Updated JSDOCS documentation in index.js

--- index.js
+++ index.js
@@ -6,12 +6,16 @@
 /**
  * Function returning Vue Mixin Object.
  * @param {Object} options - Containing prefix, actions & store parameters.
+ *
+ * prefix: {String} - OPTIONAL - String to prefix function names.
+ * actions: {Array} - REQUIRED - Redux Actions.
+ * store: {Object} - REQUIRED - Redux Store.
  */
 const reVueMixinGen = options => createMixin(options.prefix, options.actions, options.store);
 
 
 /**
- * Return a pluggable Vue mixins object
+ * Return a pluggable Vue mixins object.
  * @param {String} prefix - Name to be prepended to actions function name.
  * @param {Array} actions - Array of of functions to generate Vue methods for.
  * @param {Object} store - Redux store class.
@@ -32,4 +36,9 @@ const createMixin = (prefix, actions, store) => {
 	return {methods};
 };
 
+/**
+ * Returns a new string of prefixed property name.
+ * @param {String} prefix - option passed into function.
+ * @param {string} str -  current action name in forEach iteration.
+ */
 const prefixedPropName = (prefix, str) => prefix + str.charAt(0).toUpperCase() + str.substr(1);
